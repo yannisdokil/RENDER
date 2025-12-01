1fb691318d4d5b75a64e7d4548c5d812a188c3ad
yannisdokil <65975357+yannisdokil@users.noreply.github.com>
2025-12-01 19:47:02 +0200

    Add files via upload

---
+++ TorrServer/ApiController.cs
@@ -0,0 +1,400 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Shared;
+using Shared.Engine;
+using Shared.Models.Base;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TorrServer.Controllers
+{
+    public class TorrServerController : BaseController
+    {
+        #region ts.js
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ts.js")]
+        [Route("ts/js/{token}")]
+        public ActionResult Plugin(string token)
+        {
+            string file = FileCache.ReadAllText("plugins/ts.js").Replace("{localhost}", Regex.Replace(host, "^https?://", ""));
+
+            if (!string.IsNullOrEmpty(token))
+                file = Regex.Replace(file, "Lampa.Storage.set\\('torrserver_login'[^\n\r]+", $"Lampa.Storage.set('torrserver_login','{HttpUtility.UrlEncode(token)}');");
+
+            return Content(file, "application/javascript; charset=utf-8");
+        }
+        #endregion
+
+        #region HttpClient
+        private static readonly HttpClient httpClient = new HttpClient(new SocketsHttpHandler
+        {
+            AllowAutoRedirect = true,
+            AutomaticDecompression = DecompressionMethods.None,
+            SslOptions = { RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true },
+            MaxConnectionsPerServer = 100
+        })
+        {
+            BaseAddress = new Uri($"http://{AppInit.conf.listen.localhost}:{ModInit.tsport}"),
+            DefaultRequestHeaders =
+            {
+                Authorization = new AuthenticationHeaderValue("Basic", CrypTo.Base64($"ts:{ModInit.tspass}")),
+            },
+            Timeout = TimeSpan.FromSeconds(30)
+        };
+        #endregion
+
+
+        #region Main
+        [Route("ts")]
+        [Route("ts/static/js/{suffix}")]
+        async public Task<ActionResult> Main()
+        {
+            string html = null;
+            string pathRequest = Regex.Replace(HttpContext.Request.Path.Value, "^/ts", "");
+
+            try
+            {
+                var responseMessage = await httpClient.GetAsync(pathRequest + HttpContext.Request.QueryString.Value).ConfigureAwait(false);
+                html = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+            catch { }
+
+            if (html == null)
+                return StatusCode(500);
+
+            if (pathRequest.Contains(".js"))
+            {
+                string key = Regex.Match(html, "\\.concat\\(([^,]+),\"/echo\"").Groups[1].Value;
+                html = html.Replace($".concat({key},\"/", $".concat({key},\"/ts/");
+                return Content(html, "application/javascript; charset=utf-8");
+            }
+            else
+            {
+                html = html.Replace("href=\"/", "href=\"/ts/").Replace("src=\"/", "src=\"/ts/");
+                html = html.Replace("src=\"./", "src=\"/ts/");
+                return Content(html, "text/html; charset=utf-8");
+            }
+        }
+        #endregion
+
+        #region TorAPI
+        [Route("ts/{*suffix}")]
+        async public Task Index()
+        {
+            if (HttpContext.Request.Path.Value.StartsWith("/shutdown"))
+            {
+                HttpContext.Response.StatusCode = 404;
+                return;
+            }
+
+            if (AppInit.conf.accsdb.enable)
+            {
+                #region Обработка stream потока
+                if (HttpContext.Request.Method == "GET" && Regex.IsMatch(HttpContext.Request.Path.Value, "^/ts/(stream|play)"))
+                {
+                    await TorAPI().ConfigureAwait(false);
+                    return;
+
+                    //if (ModInit.clientIps.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+                    //{
+                    //    await TorAPI();
+                    //    return;
+                    //}
+                    //else
+                    //{
+                    //    HttpContext.Response.StatusCode = 404;
+                    //    return;
+                    //}
+                }
+                #endregion
+
+                #region Access-Control-Request-Headers
+                if (HttpContext.Request.Method == "OPTIONS" && HttpContext.Request.Headers.TryGetValue("Access-Control-Request-Headers", out var AccessControl) && AccessControl == "authorization")
+                {
+                    HttpContext.Response.StatusCode = 204;
+                    return;
+                }
+                #endregion
+
+                if (HttpContext.Request.Headers.TryGetValue("Authorization", out var Authorization))
+                {
+                    byte[] data = Convert.FromBase64String(Authorization.ToString().Replace("Basic ", ""));
+                    string[] decodedString = Encoding.UTF8.GetString(data).Split(":");
+
+                    string login = decodedString[0].ToLower().Trim();
+                    string passwd = decodedString[1];
+
+                    if (AppInit.conf.accsdb.findUser(login) is AccsUser user && !user.ban && user.expires > DateTime.UtcNow && passwd == ModInit.conf.defaultPasswd)
+                    {
+                        if (ModInit.conf.group > user.group)
+                        {
+                            await HttpContext.Response.WriteAsync("NoAccessGroup", HttpContext.RequestAborted).ConfigureAwait(false);
+                            return;
+                        }
+
+                        await TorAPI(user).ConfigureAwait(false);
+                        return;
+                    }
+                }
+
+                if (HttpContext.Request.Path.Value.StartsWith("/ts/echo"))
+                {
+                    await HttpContext.Response.WriteAsync("MatriX.API", HttpContext.RequestAborted).ConfigureAwait(false);
+                    return;
+                }
+
+                HttpContext.Response.StatusCode = 401;
+                HttpContext.Response.Headers["Www-Authenticate"] = "Basic realm=Authorization Required";
+                return;
+            }
+            else
+            {
+                await TorAPI().ConfigureAwait(false);
+                return;
+            }
+        }
+
+        async public Task TorAPI(AccsUser user = null)
+        {
+            string pathRequest = Regex.Replace(HttpContext.Request.Path.Value, "^/ts", "");
+            string servUri = $"http://{AppInit.conf.listen.localhost}:{ModInit.tsport}{pathRequest + HttpContext.Request.QueryString.Value}";
+
+            #region settings
+            if (pathRequest.StartsWith("/settings"))
+            {
+                if (HttpContext.Request.Method != "POST")
+                {
+                    HttpContext.Response.StatusCode = 404;
+                    await HttpContext.Response.WriteAsync("404 page not found", HttpContext.RequestAborted).ConfigureAwait(false);
+                    return;
+                }
+
+                using (var reader = new StreamReader(HttpContext.Request.Body, Encoding.UTF8, leaveOpen: true))
+                {
+                    string requestJson = await reader.ReadToEndAsync().ConfigureAwait(false);
+
+                    if (requestJson.Contains("\"get\""))
+                    {
+                        var rs = await httpClient.PostAsync("/settings", new StringContent("{\"action\":\"get\"}", Encoding.UTF8, "application/json")).ConfigureAwait(false);
+                        await rs.Content.CopyToAsync(HttpContext.Response.Body, HttpContext.RequestAborted).ConfigureAwait(false);
+                        return;
+                    }
+                    else if (!ModInit.conf.rdb || requestInfo.IP == "127.0.0.1" || requestInfo.IP.StartsWith("192.168."))
+                    {
+                        await httpClient.PostAsync("/settings", new StringContent(requestJson, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+                    }
+
+                    await HttpContext.Response.WriteAsync(string.Empty, HttpContext.RequestAborted).ConfigureAwait(false);
+                    return;
+                }
+            }
+            #endregion
+
+            #region playlist
+            if (pathRequest.StartsWith("/stream/") && HttpContext.Request.QueryString.Value.Contains("&m3u"))
+            {
+                string m3u = await httpClient.GetStringAsync(servUri).ConfigureAwait(false);
+                HttpContext.Response.ContentType = "audio/x-mpegurl; charset=utf-8";
+                await HttpContext.Response.WriteAsync((m3u ?? string.Empty).Replace("/stream/", "/ts/stream/"), HttpContext.RequestAborted).ConfigureAwait(false);
+                return;
+            }
+            #endregion
+
+            #region multiaccess
+            if (ModInit.conf.multiaccess == "full" || (ModInit.conf.multiaccess == "auth" && user != null))
+            {
+                if (HttpContext.Request.Method == "POST" && pathRequest == "/torrents" && user?.group != 666)
+                {
+                    HttpContext.Request.EnableBuffering();
+                    using (var readerBody = new StreamReader(HttpContext.Request.Body, Encoding.UTF8, leaveOpen: true)) // Оставляем поток открытым
+                    {
+                        string requestJson = await readerBody.ReadToEndAsync().ConfigureAwait(false);
+
+                        if (requestJson.Contains("\"action\":\"add\"") || requestJson.Contains("\"action\":\"list\""))
+                        {
+                            try
+                            {
+                                var rs = await httpClient.PostAsync(pathRequest, new StringContent(requestJson, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+                                string json = await rs.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                                string uid = user?.id ?? user?.ids?.FirstOrDefault();
+                                HttpContext.Response.ContentType = "application/json; charset=utf-8";
+
+                                if (requestJson.Contains("\"action\":\"add\""))
+                                {
+                                    #region add
+                                    string hash = Regex.Match(json, "\"hash\":\"([^\"]+)\"").Groups[1].Value;
+                                    if (!string.IsNullOrEmpty(hash))
+                                    {
+                                        var doc = ModInit.whosehash.FindById(hash);
+
+                                        if (doc != null)
+                                        {
+                                            doc.ip = requestInfo.IP;
+                                            doc.uid = uid;
+                                            ModInit.whosehash.Update(doc);
+                                        }
+                                        else
+                                        {
+                                            ModInit.whosehash.Insert(new WhoseHashModel
+                                            {
+                                                id = hash,
+                                                ip = requestInfo.IP,
+                                                uid = uid
+                                            });
+                                        }
+                                    }
+
+                                    await HttpContext.Response.WriteAsync(json, HttpContext.RequestAborted).ConfigureAwait(false);
+                                    return;
+                                    #endregion
+                                }
+                                else
+                                {
+                                    #region list
+                                    var torrents = JArray.Parse(json);
+
+                                    for (int i = torrents.Count - 1; i >= 0; i--)
+                                    {
+                                        var hash = torrents[i]["hash"]?.ToString();
+
+                                        if (!string.IsNullOrEmpty(hash))
+                                        {
+                                            var doc = ModInit.whosehash.FindById(hash);
+
+                                            if (doc != null)
+                                            {
+                                                if (doc.ip == requestInfo.IP || (doc.uid != null && doc.uid == uid)) { }
+                                                else
+                                                    torrents.RemoveAt(i);
+                                            }
+                                        }
+                                    }
+
+                                    await HttpContext.Response.WriteAsync(torrents.ToString(), HttpContext.RequestAborted).ConfigureAwait(false);
+                                    return;
+                                    #endregion
+                                }
+                            }
+                            catch { }
+
+                            HttpContext.Response.StatusCode = 500;
+                            await HttpContext.Response.WriteAsync(string.Empty, HttpContext.RequestAborted).ConfigureAwait(false);
+                            return;
+                        }
+                    }
+
+                    // Сбрасываем позицию
+                    HttpContext.Request.Body.Position = 0;
+                }
+            }
+            #endregion
+
+            var request = CreateProxyHttpRequest(HttpContext, new Uri(servUri));
+
+            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+            await CopyProxyHttpResponse(HttpContext, response).ConfigureAwait(false);
+        }
+        #endregion
+
+
+        #region CreateProxyHttpRequest
+        HttpRequestMessage CreateProxyHttpRequest(HttpContext context, Uri uri)
+        {
+            var request = context.Request;
+
+            var requestMessage = new HttpRequestMessage();
+            var requestMethod = request.Method;
+            if (HttpMethods.IsPost(requestMethod))
+            {
+                var streamContent = new StreamContent(request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            foreach (var header in request.Headers)
+            {
+                if (header.Key.ToLower() is "authorization")
+                    continue;
+
+                if (!requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray()) && requestMessage.Content != null)
+                    requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+
+            requestMessage.Headers.Host = string.IsNullOrEmpty(AppInit.conf.listen.host) ? context.Request.Host.Value : AppInit.conf.listen.host;
+            requestMessage.RequestUri = uri;
+            requestMessage.Method = new HttpMethod(request.Method);
+
+            return requestMessage;
+        }
+        #endregion
+
+        #region CopyProxyHttpResponse
+        async Task CopyProxyHttpResponse(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            var response = context.Response;
+            response.StatusCode = (int)responseMessage.StatusCode;
+
+            #region UpdateHeaders
+            void UpdateHeaders(HttpHeaders headers)
+            {
+                foreach (var header in headers)
+                {
+                    if (header.Key.ToLower() is "transfer-encoding" or "etag" or "connection" or "content-security-policy" or "content-disposition")
+                        continue;
+
+                    string value = string.Empty;
+                    foreach (var val in header.Value)
+                        value += $"; {val}";
+
+                    response.Headers[header.Key] = Regex.Replace(value, "^; ", "");
+                    //response.Headers[header.Key] = header.Value.ToArray();
+                }
+            }
+            #endregion
+
+            UpdateHeaders(responseMessage.Headers);
+            UpdateHeaders(responseMessage.Content.Headers);
+
+            var responseStream = await responseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+            if (response.Body == null)
+                throw new ArgumentNullException("destination");
+
+            if (!responseStream.CanRead && !responseStream.CanWrite)
+                throw new ObjectDisposedException("ObjectDisposed_StreamClosed");
+
+            if (!response.Body.CanRead && !response.Body.CanWrite)
+                throw new ObjectDisposedException("ObjectDisposed_StreamClosed");
+
+            if (!responseStream.CanRead || !response.Body.CanWrite)
+                throw new NotSupportedException("NotSupported_UnreadableStream");
+
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(4096);
+
+            try
+            {
+                int bytesRead;
+
+                while ((bytesRead = await responseStream.ReadAsync(buffer, context.RequestAborted).ConfigureAwait(false)) != 0)
+                    await response.Body.WriteAsync(buffer, 0, bytesRead, context.RequestAborted).ConfigureAwait(false);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+        #endregion
+    }
+}
---
+++ TorrServer/ModInit.cs
@@ -0,0 +1,271 @@
+﻿using LiteDB;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Shared.Engine;
+using Shared.Models.Module;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TorrServer
+{
+    public class ModInit
+    {
+        #region static
+        static bool IsShutdown;
+
+        public static int tsport = 9080;
+
+        public static string tspass = CrypTo.md5(DateTime.Now.ToBinary().ToString());
+
+        public static string homedir;
+
+        public static string tspath;
+
+        public static Process tsprocess;
+        #endregion
+
+        #region dataDb
+        static LiteDatabase dataDb;
+
+        public static ILiteCollection<WhoseHashModel> whosehash { get; set; }
+        #endregion
+
+        #region ModInit
+        public string releases { get; set; } = "MatriX.135";
+
+        public bool rdb { get; set; }
+
+        public string defaultPasswd { get; set; } = "ts";
+
+        public int group { get; set; }
+
+        /// <summary>
+        /// auth
+        /// full
+        /// null - desable
+        /// </summary>
+        public string multiaccess { get; set; } = "auth";
+
+        public bool checkfile { get; set; } = true;
+
+
+        static (ModInit, DateTime) cacheconf = default;
+
+        public static ModInit conf => cacheconf.Item1;
+        #endregion
+
+        #region cron_UpdateSettings
+        static Timer _cronTimer;
+
+        static bool _cronWork = false;
+
+        static void cron_UpdateSettings(object state)
+        {
+            if (_cronWork)
+                return;
+
+            _cronWork = true;
+
+            try
+            {
+                string path = "module/TorrServer.conf";
+
+                if (!File.Exists(path))
+                {
+                    if (cacheconf.Item1 == null)
+                        cacheconf.Item1 = new ModInit();
+
+                    return;
+                }
+
+                var lastWriteTime = File.GetLastWriteTime(path);
+
+                if (cacheconf.Item2 != lastWriteTime)
+                {
+                    cacheconf.Item1 = JsonConvert.DeserializeObject<ModInit>(File.ReadAllText(path));
+                    cacheconf.Item2 = lastWriteTime;
+                }
+            }
+            catch { }
+            finally
+            {
+                _cronWork = false;
+            }
+        }
+        #endregion
+
+        #region loaded
+        public static void loaded(InitspaceModel initspace)
+        {
+            RegisterShutdown(initspace);
+
+            _cronTimer = new Timer(cron_UpdateSettings, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+            dataDb = new LiteDatabase("cache/ts.db");
+            whosehash = dataDb.GetCollection<WhoseHashModel>("whosehash");
+
+            #region homedir
+            homedir = Directory.GetCurrentDirectory();
+            if (string.IsNullOrWhiteSpace(homedir) || homedir == "/")
+                homedir = string.Empty;
+
+            homedir = Path.Combine(homedir, "torrserver");
+            Directory.CreateDirectory(homedir);
+            #endregion
+
+            #region tspath
+            tspath = Path.Combine(homedir, "TorrServer-linux");
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                tspath = Path.Combine(homedir, "TorrServer-windows-amd64.exe");
+            #endregion
+
+            File.WriteAllText(Path.Combine(homedir, "accs.db"), $"{{\"ts\":\"{tspass}\"}}");
+
+            ThreadPool.QueueUserWorkItem(async _ =>
+            {
+                #region downloadUrl
+                string downloadUrl;
+                if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                {
+                    downloadUrl = "https://github.com/YouROK/TorrServer/releases/latest/download/TorrServer-windows-amd64.exe";
+                    if (conf.releases != "latest")
+                        downloadUrl = $"https://github.com/YouROK/TorrServer/releases/download/{conf.releases}/TorrServer-windows-amd64.exe";
+                }
+                else
+                {
+                    string uname = (await Bash.Run("uname -m")) ?? string.Empty;
+                    string arch = uname.Contains("x86_64") ? "amd64" : (uname.Contains("i386") || uname.Contains("i686")) ? "386" : uname.Contains("aarch64") ? "arm64" : uname.Contains("armv7") ? "arm7" : uname.Contains("armv6") ? "arm5" : "amd64";
+
+                    downloadUrl = "https://github.com/YouROK/TorrServer/releases/latest/download/TorrServer-linux-" + arch;
+                    if (conf.releases != "latest")
+                        downloadUrl = $"https://github.com/YouROK/TorrServer/releases/download/{conf.releases}/TorrServer-linux-" + arch;
+                }
+                #endregion
+
+                #region updatet/install
+                async Task install()
+                {
+                    try
+                    {
+                        if (conf.releases == "latest")
+                        {
+                            var root = await Http.Get<JObject>("https://api.github.com/repos/YouROK/TorrServer/releases/latest");
+                            if (root != null && root.ContainsKey("tag_name"))
+                            {
+                                string tagname = root.Value<string>("tag_name");
+                                if (!string.IsNullOrEmpty(tagname))
+                                {
+                                    if (!File.Exists($"{homedir}/tagname") || tagname != File.ReadAllText($"{homedir}/tagname"))
+                                    {
+                                        if (File.Exists(tspath))
+                                            File.Delete(tspath);
+
+                                        File.WriteAllText($"{homedir}/tagname", tagname);
+                                    }
+                                }
+                            }
+                        }
+
+                        if (!File.Exists(tspath))
+                        {
+                            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                            {
+                                tsprocess?.Dispose();
+                                bool success = await Http.DownloadFile(downloadUrl, tspath, timeoutSeconds: 200);
+                                if (!success)
+                                    File.Delete(tspath);
+                            }
+                            else
+                            {
+                                tsprocess?.Dispose();
+                                bool success = await Http.DownloadFile(downloadUrl, tspath, timeoutSeconds: 200);
+                                if (success)
+                                    Bash.Invoke($"chmod +x {tspath}");
+                                else
+                                    Bash.Invoke($"rm -f {tspath}");
+                            }
+                        }
+                    }
+                    catch { }
+                }
+
+                await install();
+
+                if (!File.Exists(tspath))
+                {
+                    await Task.Delay(10_000);
+                    await install();
+                }
+
+                if (!File.Exists("isdocker") && conf.checkfile)
+                {
+                    var response = await Http.ResponseHeaders(downloadUrl, timeoutSeconds: 10, allowAutoRedirect: true);
+                    if (response != null && response.Content.Headers.ContentLength.HasValue && new FileInfo(tspath).Length != response.Content.Headers.ContentLength.Value)
+                    {
+                        File.Delete(tspath);
+                        await Task.Delay(10_000);
+                        await install();
+                    }
+                }
+                #endregion
+
+                while (!IsShutdown && File.Exists(tspath))
+                {
+                    try
+                    {
+                        tsprocess = new Process();
+                        tsprocess.StartInfo.UseShellExecute = false;
+                        tsprocess.StartInfo.RedirectStandardOutput = true;
+                        tsprocess.StartInfo.RedirectStandardError = true;
+                        tsprocess.StartInfo.FileName = tspath;
+                        tsprocess.StartInfo.Arguments = $"--httpauth -p {tsport} -d \"{homedir}\"";
+
+                        tsprocess.Start();
+
+                        tsprocess.OutputDataReceived += (sender, args) => { };
+                        tsprocess.ErrorDataReceived += (sender, args) => { };
+                        tsprocess.BeginOutputReadLine();
+                        tsprocess.BeginErrorReadLine();
+
+                        await tsprocess.WaitForExitAsync();
+                    }
+                    catch { }
+
+                    await Task.Delay(10_000);
+                }
+            });
+        }
+        #endregion
+
+
+        #region Shutdown
+        static void RegisterShutdown(InitspaceModel initspace)
+        {
+            if (initspace?.app?.ApplicationServices != null)
+            {
+                var lifetime = initspace.app.ApplicationServices.GetService<IHostApplicationLifetime>();
+                lifetime?.ApplicationStopping.Register(StopTranscoding);
+            }
+
+            AppDomain.CurrentDomain.ProcessExit += (_, _) => StopTranscoding();
+        }
+
+        static void StopTranscoding()
+        {
+            try
+            {
+                IsShutdown = true;
+                tsprocess?.Kill(true);
+                _cronTimer.Dispose();
+            }
+            catch { }
+        }
+        #endregion
+    }
+}
